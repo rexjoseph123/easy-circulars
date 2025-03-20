3269dc87c925048784d2af99a783022ab0be9012
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2025-03-20 14:51:23 +0530

    Reduce headers

--- webscraper/urlscraper.py
+++ webscraper/urlscraper.py
@@ -81,23 +81,9 @@ class URLScraper:
                 }
 
                 headers = {
-                    "Host": "rbi.org.in",
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                    "Accept-Language": "en-US,en;q=0.5",
-                    "Accept-Encoding": "gzip, deflate, br, zstd",
                     "Content-Type": "application/x-www-form-urlencoded",
-                    "Origin": "https://rbi.org.in",
-                    "DNT": "1",
-                    "Connection": "keep-alive",
-                    "Referer": url,
-                    "Upgrade-Insecure-Requests": "1",
-                    "Sec-Fetch-Dest": "document",
-                    "Sec-Fetch-Mode": "navigate",
-                    "Sec-Fetch-Site": "same-origin",
-                    "Sec-Fetch-User": "?1",
-                    "Priority": "u=0, i",
-                    "TE": "trailers",
+                    "Referer": self.url,
                 }
 
                 post_response = self.session.post(url, headers=headers, cookies=cookies, data=payload)
